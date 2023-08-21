7c9ba49ffbb5ec67b6a9b8acff225fdc102e7486
lautaroleonhardt <lautaro.leonhardt01@gmail.com>
2023-08-21 18:52:07 -0300

    chore: add user data file

---
+++ tests/e2e/data/users.data.ts
@@ -0,0 +1,10 @@
+import { User } from '../Entities/User'
+
+export const validUser: User = {
+  firstName: 'Lautaro',
+  lastName: 'Leonhardt',
+  email: 'lautaro.leonhardt01@gmail.com',
+  password: '123456',
+  location: 'My city',
+  sex: 'male',
+}
