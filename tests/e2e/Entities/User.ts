1eef87cc0c0171d6a14d37522dd8980b5e2ae2d7
lautaroleonhardt <lautaro.leonhardt01@gmail.com>
2023-09-13 17:11:07 -0300

    chore: add User.ts interfaces

--- tests/e2e/Entities/User.ts
+++ tests/e2e/Entities/User.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 
-type sex = 'male' | 'female'
+type sex = 'male' | 'female' | undefined
 
 export interface User {
   firstName: string
@@ -11,13 +11,35 @@ export interface User {
   location: string
 }
 
+export interface UserProps {
+  sex?: 'male' | 'female' | undefined
+  password?: PasswordOptions
+  firstName?: sex
+  lastName?: sex
+  email?: EmailOptions
+}
+
+interface PasswordOptions {
+  length?: number
+  memorable?: boolean
+  pattern?: RegExp
+  prefix?: string
+}
+
+interface EmailOptions {
+  firstName: string
+  lastName: string
+  provider: string
+  allowSpecialCharacters: boolean
+}
+
 export class User implements User {
-  constructor(sex?: sex) {
-    this.firstName = faker.person.firstName(sex)
-    this.lastName = faker.person.lastName()
-    this.email = `${this.firstName}.${this.lastName}-${faker.number.int({ min: 0, max: 999999 })}@test.com`
-    this.password = faker.internet.password({ length: 8, memorable: true })
-    this.sex = sex || (faker.person.sex() as sex)
+  constructor(userProps?: UserProps) {
+    this.firstName = faker.person.firstName(userProps?.sex)
+    this.lastName = faker.person.lastName(userProps?.lastName)
+    this.email = faker.internet.email(userProps?.email)
+    this.password = faker.internet.password(userProps?.password)
+    this.sex = userProps?.sex || (faker.person.sex() as sex)
     this.location = faker.location.city()
   }
 }
